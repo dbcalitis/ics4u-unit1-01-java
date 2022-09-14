43359c1a1d9bce9b72dc4e05dc42fc37916b3c79
dbcalitis <daria.bernice.calitis@mths.ca>
2022-09-14 20:26:23 +0000

    updated Logs.java

--- Logs.java
+++ Logs.java
@@ -14,44 +14,44 @@ import java.util.Scanner;
 * .Class constructor Logs.
 */
 final class Logs {
-        /**
-        * Prevent instantiation.
-        * Throw an exception IllegalStateException.
-        * if this ever is called
-        *
-        * @throws IllegalStateException
-        *
-        */
-	private Logs() {
-		throw new IllegalStateException("Cannot be instantiated");
-	}
+    /**
+     * Prevent instantiation.
+     * Throw an exception IllegalStateException
+     * if this ever is called
+     *
+     * @throws IllegalStateException
+     *
+     */
+    private Logs() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
 
-	/**
-	 * The starting main() function.
-	 *
-	 * @param args No args will be used
-	 */
-	public static void main(final String[] args) {
-		// Create Scanner Object
-		final Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the length of the maple logs (m): ");
+    /**
+     * The starting main() function.
+     *
+     * @param args No args will be used
+     */
+    public static void main(final String[] args) {
+        // Create Scanner Object
+        final Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the length of the maple logs (m): ");
 
-		// Gets the length of the logs
-		final float logLength = scan.nextFloat();
+        // Gets the length of the logs
+        final float logLength = scan.nextFloat();
 
-		// Constants
-		final int weightCapacity = 1100;
-		final int logDensity = 20;
+        // Constants
+        final int WEIGHT_CAPACITY = 1100;
+        final int LOG_DENSITY = 20;
 
-		// Calculates the number of logs that could fit in the truck
-		final float numberOfLogs =
-				weightCapacity / logDensity / logLength;
+        // Calculates the number of logs that could fit in the truck
+        final float numberOfLogs =
+            WEIGHT_CAPACITY / LOG_DENSITY / logLength;
 
-		// Prints out the result
-		System.out.printf("%d logs can fit into the truck",
-		       	Math.round(numberOfLogs)
-		);
+        // Prints out the result
+        System.out.printf("%d logs can fit into the truck",
+            Math.round(numberOfLogs)
+        );
 
-		System.out.println("\n\nDone.");
-	}
+        System.out.println("\n\nDone.");
+    }
 }
